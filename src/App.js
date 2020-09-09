a90edc10c9379cc73a0ba8b88741429433f4fa5e
unknown <ritabratabasudhar@gmailcom>
2020-09-09 14:57:09 +0530

    change

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component
   return (
     <div className="App">
       <MenuBar/>
+      
       <Router history={history}>
       <Switch>
       <Route exact path= '/react-project/'component= {Homepage}/>
